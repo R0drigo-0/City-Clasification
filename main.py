f15052da597efa3c9bb6f6ef4549c6c7e563089d
Sergi Diaz Lopez <sergidiazlopez@gmail.com>
2024-05-16 12:05:11 +0200

    add results for V1, V2 and V3

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import glob
 
 import cv2
 
+import csv
+
 # from CityClassifierV1 import CityClassifier
 # from CityClassifierV2 import CityClassifier
 # from CityClassifierV3 import CityClassifier
@@ -9,18 +11,23 @@ from CityClassifierV4 import CityClassifier
 
 c = CityClassifier(show_results = False)
 # c.generate_map_descriptors()
-query = cv2.imread("./images/train/Barcelona_crop_1.png", cv2.IMREAD_GRAYSCALE)
-query = "./images/train/Barcelona_crop_1.png"
+query = cv2.imread("./images/test/Barcelona_crop_1.png", cv2.IMREAD_GRAYSCALE)
+query = "./images/test/Barcelona_crop_1.png"
 
 # result = c.classify(query)
 # print(f"Result: {result}")
 
 # Test all images in test
-test_images = glob.glob("./images/train/*.png")
-for query in test_images:
-    # query = cv2.imread(im, cv2.IMREAD_GRAYSCALE)
-    result = c.classify(query)
-    print(f"Result for {query}: {result}")
+
+test_images = glob.glob("./images/test/*.png")
+with open('./results/V2.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["Input file", "Prediction"])
+    for query in test_images:
+        # query = cv2.imread(im, cv2.IMREAD_GRAYSCALE)
+        result = c.classify(query)
+        csvwriter.writerow([query, result])
+        print(f"Result for {query}: {result}")
 
 
 
